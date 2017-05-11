3a7c78f53f1c9b3da2445895d1d3c003cb2773ee
SharpAceX <sharpacex@gmail.com>
2017-05-10 22:16:10 -0400

    Styles and data added

--- javascript.js
+++ javascript.js
@@ -1,5 +1,18 @@
 Vue.component('timeline', {
+    props: ['source'],
     template: '#timeline-template',
+    data: () => ({
+        data: {}
+    }),
+    created() {
+        axios.get(this.source).then(response => {
+            this.data = response.data
+            console.log(this.data);
+        })
+        .catch(e => {
+            this.errors.push(e)
+        })
+    }
 });
 
 new Vue({
